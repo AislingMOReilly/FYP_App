de22c79baf70040feb632f9e244dbeb2bfac9981
Aisling <aisling.oreilly@mycit.ie>
2021-08-09 19:37:41 +0100

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+import firebase from "firebase";
+
+ // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCQwyNvmG1ONwSW8Lt7GF_7ZcUeUKCdUPI",
+  authDomain: "fyp-project-2e133.firebaseapp.com",
+  projectId: "fyp-project-2e133",
+  storageBucket: "fyp-project-2e133.appspot.com",
+  messagingSenderId: "133406334225",
+  appId: "1:133406334225:web:ebe1dffc348451016980b0",
+  measurementId: "G-SPPX4D0SC3"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+const usersCollection = db.collection('users');
+
+export {
+  db, 
+  auth, 
+  usersCollection
+};
