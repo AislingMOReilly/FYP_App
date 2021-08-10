e0285dd1af78e4f43435924621b498c7208408ee
Aisling <aisling.oreilly@mycit.ie>
2021-08-10 18:00:25 +0100

    nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <v-app>
+    <v-navigation-drawer temporary v-model="sideNav">
+      <v-list>
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          router
+          :to="item.link">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
@@ -32,12 +47,12 @@
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
 
-      <div id="nav">
+      <!-- <div id="nav">
         <router-link to="/">Home</router-link> |
         <router-link to="/register">Register</router-link>
         <router-link to="/dashboard">Dashboard</router-link>
       </div>
-    </v-app-bar>
+    </v-app-bar> -->
 
     <v-main>
       <router-view />
@@ -51,6 +66,16 @@ export default {
 
   data: () => ({
     //
+    return : {
+        sideNav: false,
+        menuItems: [
+          {icon: 'mdi-view-dashboard-outline', title: 'Dashboard', link: '/dashboard'},
+          {icon: 'mdi-account-circle-outline', title: 'Profile', link: '/profile'},
+          //{icon: 'mdi-account-plus', title: 'Register', link: '/register'},
+          {icon: 'mdi-pencil-plus-outline', title: 'Register', link: '/register'},
+          {icon: 'mdi-login', title: 'Sign in', link: '/login'},
+        ],
+      }
   }),
 };
 </script>
